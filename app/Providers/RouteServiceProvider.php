e4a708bc3a1c43ea1c3b662a4098da12bb81bb2e
Javier Ibanez <expertprep2015@gmail.com>
2021-03-27 22:40:51 -0400

    added my route in my routeserviceprovider.php and added my use Auth at the top of my attemptToauthenticate.php file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
+use App\Providers\RouteServiceProvider;
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
