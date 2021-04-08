e001c8ecb39235713e2866152fcef24761ae113b
Javier Ibanez <expertprep2015@gmail.com>
2021-04-08 01:14:08 -0400

    added function to my database files

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $product_name = $this->faker->unique()->words($nb=4,$asText=true);
+        $slug = Str::slug($category_name);
+        return [
+            'name' =>$product_name,
+            
+        ];
+    }
+}
